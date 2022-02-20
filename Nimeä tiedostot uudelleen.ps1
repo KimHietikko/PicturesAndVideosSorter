fc073363581e8573941c0f46818c01927d8c89c6
Kim Hietikko <kim.hietikko@gmail.com>
2022-02-20 19:53:31 +0200

    Rename files

--- Nimeä tiedostot uudelleen.ps1
+++ Nimeä tiedostot uudelleen.ps1
@@ -1,1 +1,16 @@
-Get-ChildItem *.mp4 | Rename-Item -NewName { $_.Name -replace 'Copy of','' }
+$SomeFolder= Get-ChildItem "E:\PathToFolder"
+
+$File_data = Get-Content .\pvm.txt -Encoding UTF8
+$File_dataName = Get-Content .\nimet.txt -Encoding UTF8
+
+for($file=0; $file -lt $SomeFolder.Length; $file++){ 
+    #Write-Host ($SomeFolder[$file])
+    #endregionWrite-Host ($File_data[$file])
+
+    #$SomeFolder[$file] | Rename-Item -NewName { $_.BaseName + '--' + $File_data[$file] + '--'+ $_.Extension}
+    #$SomeFolder[$file] | Rename-Item -NewName { $_.Name -replace $File_data[$file],'' }
+
+    #$SomeFolder[$file] | Rename-Item -NewName { $File_dataName[$file] + '_' + $File_data[$file] + '_'+ $_.Extension }
+}
+
+#Get-ChildItem *.mp4 | Rename-Item -NewName { $_.Name -replace 'Copy of','' }
